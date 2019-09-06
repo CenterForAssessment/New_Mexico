dfc4b3ea3cbe48db7f2e0f7dcfeb76801fdfe67e
Damian Betebenner <dbetebenner@nciea.org>
2019-09-05 21:57:53 -0400

    First commit of analysis script

---
+++ New_Mexico_SGP.R
@@ -0,0 +1,42 @@
+#########################################################################################
+###
+### R Script to create SGPs for New Mexico
+###
+#########################################################################################
+
+### Load SGP Package
+
+require(SGP)
+
+
+### Load data
+
+load("Data/New_Mexico_Data_LONG.Rdata")
+
+
+###  Load configs
+
+source("SGP_CONFIG/2018_2019/READING.R")
+source("SGP_CONFIG/2018_2019/READING_ISAT.R")
+
+New_Mexico.config <- c(READING.2018_2019.config, READING_ISAT.2018_2019.config)
+
+
+### abcSGP
+
+New_Mexico_SGP <- abcSGP(
+		New_Mexico_Data_LONG,
+		steps=c("prepareSGP", "analyzeSGP", "combineSGP", "outputSGP"),
+		sgp.config=New_Mexico.config,
+		sgp.percentiles=TRUE,
+		sgp.projections=FALSE,
+		sgp.projections.lagged=FALSE,
+		sgp.percentiles.baseline=FALSE,
+		sgp.projections.baseline=FALSE,
+		sgp.projections.lagged.baseline=FALSE,
+		simulate.sgps=FALSE,
+		parallel.config=list(BACKEND="PARALLEL", WORKERS=list(PERCENTILES=4)))
+
+#  Save the SGP Object as an .Rdata file:
+
+#save(New_Mexico_SGP, file="Data/New_Mexico_SGP.Rdata")
