6c5be12c01398e4cbcbba70f0a1dc61d20bcc919
Damian Betebenner <dbetebenner@nciea.org>
2024-08-13 13:38:27 -0400

    Adding in 2023 scripts

--- New_Mexico_Data_LONG.R
+++ New_Mexico_Data_LONG.R
@@ -1,56 +1,68 @@
-##########################################################
+########################################################################################################
 ###
-### Data prep script for New Mexico ISTATION and PARCC data
+### Data prep script for 2022 and 2023 New Mexico Measures of Student Success and Achievement (MSSA)
 ###
-##########################################################
+########################################################################################################
 
 ### Load packages
-
 require(data.table)
 
-
 ### Load data
-
-#New_Mexico_Data_LONG <- fread("Data/Base_Files/Istation_SGP.csv", colClasses=rep("character", 10))
-New_Mexico_Data_LONG <- fread("Data/Base_Files/Istation_SGP_V3.csv", colClasses=rep("character", 11))
-
+New_Mexico_Data_LONG <- fread("Data/Base_Files/New_Mexico_Data_LONG_081324.csv")
 
 ### Tidy up data
-
-New_Mexico_Data_LONG[,TestnameL:=NULL]
-setnames(New_Mexico_Data_LONG, c("ID", "YEAR", "DISTRICT_NUMBER", "SCHOOL_NUMBER", "TEST_NAME", "CONTENT_AREA", "TEST_LANGUAGE", "TEST_WINDOW", "GRADE", "SCALE_SCORE"))
-
-New_Mexico_Data_LONG[,YEAR:=as.factor(YEAR)]
-setattr(New_Mexico_Data_LONG$YEAR, "levels", c("2016_2017", "2017_2018", "2018_2019"))
-New_Mexico_Data_LONG[,YEAR:=as.character(YEAR)]
-New_Mexico_Data_LONG[TEST_NAME=="ISTAT" & TEST_WINDOW=="Fall", YEAR:=paste0(YEAR, ".1")]
-New_Mexico_Data_LONG[TEST_NAME=="ISTAT" & TEST_WINDOW=="Winter", YEAR:=paste0(YEAR, ".2")]
-New_Mexico_Data_LONG[TEST_NAME=="ISTAT" & TEST_WINDOW=="Spring", YEAR:=paste0(YEAR, ".3")]
-New_Mexico_Data_LONG[CONTENT_AREA=="READ" & TEST_NAME=="TAMELA SPRING", YEAR:=paste0(YEAR, ".3")]
-
-
-New_Mexico_Data_LONG[,DISTRICT_NUMBER:=as.numeric(DISTRICT_NUMBER)]
-New_Mexico_Data_LONG[,SCHOOL_NUMBER:=as.numeric(SCHOOL_NUMBER)]
-
-New_Mexico_Data_LONG[CONTENT_AREA=="READ" & TEST_NAME=="ISTAT" & TEST_LANGUAGE=="E", CONTENT_AREA:="READING_ISTAT"]
-New_Mexico_Data_LONG[CONTENT_AREA=="READ" & TEST_NAME=="ISTAT" & TEST_LANGUAGE=="S", CONTENT_AREA:="READING_ISTAT_SPANISH"]
-New_Mexico_Data_LONG[CONTENT_AREA=="READ" & TEST_NAME=="TAMELA SPRING", CONTENT_AREA:="ELA_SS"]
-
+New_Mexico_Data_LONG[CONTENT_AREA=="MATH", CONTENT_AREA:="MATHEMATICS"]
+setnames(New_Mexico_Data_LONG, c("GRADE", "TestGr"), c("GRADE_ENROLLED", "GRADE"))
+New_Mexico_Data_LONG[,GRADE:=as.character(GRADE)]
+New_Mexico_Data_LONG[YEAR=="2021-22", YEAR:="2022"]
+New_Mexico_Data_LONG[YEAR=="2022-23", YEAR:="2023"]
+New_Mexico_Data_LONG[,ID:=as.character(ID)]
 New_Mexico_Data_LONG[,SCALE_SCORE:=as.numeric(SCALE_SCORE)]
+New_Mexico_Data_LONG[,ACHIEVEMENT_LEVEL:=as.factor(ACHIEVEMENT_LEVEL)]
+setattr(New_Mexico_Data_LONG$ACHIEVEMENT_LEVEL, "levels", c("Novice", "Nearing Proficiency", "Proficient", "Advanced"))
+New_Mexico_Data_LONG[,ACHIEVEMENT_LEVEL:=as.character(ACHIEVEMENT_LEVEL)]
+New_Mexico_Data_LONG[Female.2==1, GENDER:="Female"]
+New_Mexico_Data_LONG[Male.3==1, GENDER:="Male"]
+New_Mexico_Data_LONG[,c("Female.2", "Male.3"):=NULL]
 
-New_Mexico_Data_LONG[,VALID_CASE:="VALID_CASE"]
+setnames(New_Mexico_Data_LONG, c("Hispanic.4", "White.5", "Black.6", "Asian.7", "Native.8", "Multirace.9"), c("HISPANIC", "WHITE", "BLACK", "ASIAN", "NATIVE", "MULTIRACE"))
+New_Mexico_Data_LONG[WHITE==1,ETHNICITY:="White"]
+New_Mexico_Data_LONG[ASIAN==1,ETHNICITY:="Asian"]
+New_Mexico_Data_LONG[BLACK==1,ETHNICITY:="Black"]
+New_Mexico_Data_LONG[HISPANIC==1,ETHNICITY:="Hispanic"]
+New_Mexico_Data_LONG[NATIVE==1,ETHNICITY:="Native"]
+New_Mexico_Data_LONG[MULTIRACE==1,ETHNICITY:="Multirace"]
 
-setcolorder(New_Mexico_Data_LONG,c(11,6,2,9,1,10,3,4,5,7,8))
+setnames(New_Mexico_Data_LONG, c("FRL.10", "DirectCert.12", "SwD.14", "EL.16","Migrant.18", "Homeless.19", "Military.20", "Foster.21"),
+    c("FREE_REDUCED_LUNCH_STATUS", "DIRECT_CERTIFICATION_STATUS", "DISABILITY_STATUS", "ENGLISH_LANGUAGE_LEARNER_STATUS", "MIGRANT_STATUS", "HOMELESS_STATUS", "MILITARY_STATUS", "FOSTER_STATUS"))
 
+New_Mexico_Data_LONG[,FREE_REDUCED_LUNCH_STATUS:=as.factor(FREE_REDUCED_LUNCH_STATUS)]
+setattr(New_Mexico_Data_LONG$FREE_REDUCED_LUNCH_STATUS, "levels", c("Free Reduced Lunch: No", "Free Reduced Lunch: Yes"))
 
-### Test for duplicates
+New_Mexico_Data_LONG[,DIRECT_CERTIFICATION_STATUS:=as.factor(DIRECT_CERTIFICATION_STATUS)]
+setattr(New_Mexico_Data_LONG$DIRECT_CERTIFICATION_STATUS, "levels", c("Direct Certification: No", "Direct Certification: Yes"))
 
-setkey(New_Mexico_Data_LONG, VALID_CASE, CONTENT_AREA, YEAR, GRADE, ID, SCALE_SCORE)
-setkey(New_Mexico_Data_LONG, VALID_CASE, CONTENT_AREA, YEAR, GRADE, ID)
-New_Mexico_Data_LONG[which(duplicated(New_Mexico_Data_LONG, by=key(New_Mexico_Data_LONG)))-1, VALID_CASE := "INVALID_CASE"]
-setkey(New_Mexico_Data_LONG, VALID_CASE, CONTENT_AREA, YEAR, GRADE, ID)
+New_Mexico_Data_LONG[,DISABILITY_STATUS:=as.factor(DISABILITY_STATUS)]
+setattr(New_Mexico_Data_LONG$DISABILITY_STATUS, "levels", c("Disability: No", "Disability: Yes"))
 
+New_Mexico_Data_LONG[,ENGLISH_LANGUAGE_LEARNER_STATUS:=as.factor(ENGLISH_LANGUAGE_LEARNER_STATUS)]
+setattr(New_Mexico_Data_LONG$ENGLISH_LANGUAGE_LEARNER_STATUS, "levels", c("English Language Learner: No", "English Language Learner: Yes"))
 
-### Save Results
+New_Mexico_Data_LONG[,MIGRANT_STATUS:=as.factor(MIGRANT_STATUS)]
+setattr(New_Mexico_Data_LONG$MIGRANT_STATUS, "levels", c("Migrant: No", "Migrant: Yes"))
 
+New_Mexico_Data_LONG[,HOMELESS_STATUS:=as.factor(HOMELESS_STATUS)]
+setattr(New_Mexico_Data_LONG$HOMELESS_STATUS, "levels", c("Homeless: No", "Homeless: Yes"))
+
+New_Mexico_Data_LONG[,MILITARY_STATUS:=as.factor(MILITARY_STATUS)]
+setattr(New_Mexico_Data_LONG$MILITARY_STATUS, "levels", c("Military: No", "Military: Yes"))
+
+New_Mexico_Data_LONG[,FOSTER_STATUS:=as.factor(FOSTER_STATUS)]
+setattr(New_Mexico_Data_LONG$FOSTER_STATUS, "levels", c("Foster: No", "Foster: Yes"))
+
+New_Mexico_Data_LONG[ExclAccountabiltyReason=="", ExclAccountabiltyReason:=as.character(NA)]
+
+setcolorder(New_Mexico_Data_LONG, c(1,2,3,23,4,6:22,5,24:27))
+
+### Save results
 save(New_Mexico_Data_LONG, file="Data/New_Mexico_Data_LONG.Rdata")
--- New_Mexico_SGP.R
+++ New_Mexico_SGP.R
@@ -1,48 +1,31 @@
-#########################################################################################
+###################################################################
 ###
-### R Script to create SGPs for New Mexico
+### R script for 2023 SGP analyses for NM MSSA
 ###
-#########################################################################################
+###################################################################:w
 
-### Load SGP Package
-
+### Load packages
 require(SGP)
-
+require(data.table)
 
 ### Load data
-
 load("Data/New_Mexico_Data_LONG.Rdata")
 
+### Parameters
+parallel.config <- list(BACKEND="PARALLEL", WORKERS=list(PERCENTILES=4, BASELINE_PERCENTILES=4, PROJECTIONS=4, LAGGED_PROJECTIONS=4, SGP_SCALE_SCORE_TARGETS=4))
 
-### Customize SGPstateData
-
-SGPstateData[['NM']][['SGP_Configuration']] <- NULL
-
-
-###  Load configs
-
-source("SGP_CONFIG/2018_2019/ELA_SS.R")
-source("SGP_CONFIG/2018_2019/READING_ISTAT.R")
-
-New_Mexico.config <- c(ELA_SS.2018_2019.config, READING_ISTAT.2018_2019.config)
-
-
-### abcSGP
-
+### Run abcSGP
 New_Mexico_SGP <- abcSGP(
-		New_Mexico_Data_LONG,
-		steps=c("prepareSGP", "analyzeSGP", "combineSGP", "outputSGP"),
-		sgp.config=New_Mexico.config,
-		sgp.percentiles=TRUE,
-		sgp.projections=FALSE,
-		sgp.projections.lagged=FALSE,
-		sgp.percentiles.baseline=FALSE,
-		sgp.projections.baseline=FALSE,
-		sgp.projections.lagged.baseline=FALSE,
-		simulate.sgps=FALSE)#,
-#		parallel.config=list(BACKEND="PARALLEL", WORKERS=list(PERCENTILES=4)))
+        sgp_object = New_Mexico_Data_LONG,
+        steps = c("prepareSGP", "analyzeSGP", "combineSGP", "outputSGP"),
+        sgp.percentiles = TRUE,
+        sgp.projections = TRUE,
+        sgp.projections.lagged = TRUE,
+        sgp.percentiles.baseline = FALSE,
+        sgp.projections.baseline = FALSE,
+        sgp.projections.lagged.baseline = FALSE,
+        parallel.config = parallel.config
+)
 
-
-#  Save the SGP Object as an .Rdata file:
-
+### Save results
 save(New_Mexico_SGP, file="Data/New_Mexico_SGP.Rdata")
